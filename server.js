5e3266669ccf36f15920eed1623a31d490c27750
Vitalii Panasenko <presss743@gmail.com>
2026-07-27 00:40:28 +0300

    Configure server port for hosting

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get("/api", (req, res) => {
     res.json({ message: "Royal Garage API працює 🚗" });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
